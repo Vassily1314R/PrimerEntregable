530c9c3969b9f1cf71d45b7e3add5039318c56e1
Vassily1314r <amilkar.f.p@gmail.com>
2025-06-29 16:51:07 -0500

    Refactor CardProduct component to include product data structure

--- src/components/design3/CardProduct.jsx
+++ src/components/design3/CardProduct.jsx
@@ -1,7 +1,20 @@
 export const CardProduct = () => {
+  const productos = [
+    {
+      img: "",
+      title: "SINGLE TOILET PAPER ROLL",
+      subtitle: true,
+      price: 9.99,
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    },
+  ];
+
   return (
     <>
-      <div>CardProduct</div>
+      <div>
+        <img src="https://i.pravatar.cc/300" alt="Product" />
+      </div>
     </>
   );
 };
