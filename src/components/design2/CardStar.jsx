2f65c4d41d7d8261738e565c671b83f93652684d
Vassily1314r <amilkar.f.p@gmail.com>
2025-06-29 16:24:31 -0500

    Add additional user data to CardStar component

--- src/components/design2/CardStar.jsx
+++ src/components/design2/CardStar.jsx
@@ -21,14 +21,20 @@ export const CardStar = () => {
       calificacion: "⭐️⭐️⭐️⭐️⭐️",
       descripcion: "Fácil de aprender y progresivo",
     },
+    {
+      pfp: "https://i.pravatar.cc/300",
+      nombre: "Astrid",
+      calificacion: "⭐️⭐️⭐️",
+      descripcion: "Accesible, facil, escalable",
+    },
   ];
 
   return (
     <>
       {usuarios.map((elem) => {
         return (
-          <div className={style.contenedor}>
-            <img src="" alt="random image" />
+          <div className={style.contenedor} key={elem.nombre}>
+            <img src={elem.pfp} alt="random image" />
             <TextCard
               nombre={elem.nombre}
               calificacion={elem.calificacion}
